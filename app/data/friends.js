13879901e07bfd19a1d00d2f17607af23a05b71c
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-27 15:32:24 -0400

    added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,20 +2,35 @@ var friends = [
 
 {
   name :"John",
-  photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores:[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ]
+  photo :"http://images.pocketgamer.co.uk/FCKEditorFiles//familyguypeter2.jpg",
+  scores:[5,1,4,4,5,1,2,5,4,3]
 }
+
+{
+  name :"Alice",
+  photo :"http://www.miicharacters.com/miis/large/924_lilo.jpg",
+  scores:[5,1,4,1,5,2,2,2,4,3]
+}
+
+{
+  name :"Larry",
+  photo :"http://www.miicharacters.com/miis/large/2184_phineas.jpg",
+  scores:[1,1,3,4,4,1,2,2,4,1]
+}
+
+{
+  name :"Lisa",
+  photo :"https://s-media-cache-ak0.pinimg.com/236x/2e/31/c8/2e31c818f501e38b77f048363c8c613e.jpg",
+  scores:[5,2,2,4,5,2,2,1,4,4]
+}
+
+{
+  name :"Ralph",
+  photo :"http://www.miicharacters.com/miis/large/13715_mario.jpg",
+  scores:[5,1,5,4,2,1,3,1,2,3]
+}
+
+
 
 ]
 
