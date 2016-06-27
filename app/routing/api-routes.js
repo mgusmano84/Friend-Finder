2ca5915454525bd75d34bf06bb896e280395d453
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-27 15:20:16 -0400

    api routes updates

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,3 +1,25 @@
-var path 			= require('path');
-var bodyParser = require('body-parser');
+// var path 			= require('path');
+// var bodyParser = require('body-parser');
 
+
+
+//This will link the data in friends.js
+var friendsData = require('../data/friends.js');
+
+
+
+//Routing
+module.exports = function(app) {
+	
+	// This will alow you to to see the JSON data of different friends
+	app.get('/api/friends', function(req, res) {
+		res.json(friendsData);
+	});
+
+	
+	
+	app.post('api/friends', function(req, res) {
+		
+	});
+
+}
