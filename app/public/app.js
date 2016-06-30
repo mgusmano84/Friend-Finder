6520f2d002290d5131035bf7c84d00f296ef0389
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-30 12:40:22 -0400

    friend entry pushing to array

--- app/public/app.js
+++ app/public/app.js
@@ -18,15 +18,23 @@ $(document).ready(function() {
 
    		var newFriendData = {
       		name: $('#name').val().trim(),
-      		photo: $('#photo').val(),
-			results: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]
+      		photo: $('#image').val().trim(),
+			scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]
 		};
 		console.log(newFriendData)
-    });
+    
+
+    $.post("/api/friends", newFriendData,
+	    function(){
+
+	    	//reset the name and image
+	        $('#name').val("");
+			$('#image').val("");
+		});
 
 
 
-	
+	});
 
 
 });
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,22 +1,29 @@
 //This will link the data in friends.js
 var friendsData = require('../data/friends.js');
 var path 			= require('path');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 
 
 //Routing
 module.exports = function(app) {
+	
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.text());
+	app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
+	//Routing for friends.js
 	app.get('/api/friends', function(req, res) {
 		res.json((friendsData));
 	});
 
 	
-	
-	app.post('api/friends', function(req, res) {
-		
+	//This pushes new data into the friends array
+	app.post('/api/friends', function(req, res) {
+		friendsData.push(req.body);
+		res.json(true);
 	});
 
 }
