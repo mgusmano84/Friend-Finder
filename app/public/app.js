e3a865e1615c93cc04bf0627351e8942901cdc4d
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-30 12:06:00 -0400

    array pushed to console on click

--- app/public/app.js
+++ app/public/app.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
 
-$('.questions').select2({
+	$('.questions').select2({
 	placeholder: "Select an Option"
+	})
+	
+	
+
+	// var user = {};
+	// var userScore = [];
+
+	// var y = 0;
+	// var matchName;
+ //  	var matchImage;
+
+
+	$('.butt2').on('click', function(){
+
+   		var newFriendData = {
+      		name: $('#name').val().trim(),
+      		photo: $('#photo').val(),
+			results: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]
+		};
+		console.log(newFriendData)
+    });
+
+
+
+	
+
+
 });
 
 
 
 
-})
 
 
+
+		
